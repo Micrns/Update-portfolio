075805e9b43ee9987c00fe9381b0501774585824
Oscar <ovreyes@uci.edu>
2024-06-09 23:06:08 -0700

    update-to-homepage

--- Portfolio/components/layout/Introduction.js
+++ Portfolio/components/layout/Introduction.js
@@ -1,41 +1,19 @@
-import Image from "next/image"
 import NavBar from "@/components/layout/Navigation";
 
-
-
-
-
-export default function Introduct () {
-
-    return(
-
+export default function Introduct() {
+    return (
         <section className="relative h-screen">
             <NavBar />
-            <div className="absolute inset-0">
-                <Image
-                    src="/IMG_0591.jpg"
-                    layout="fill"
-                    objectFit="cover"
-                    quality={100}
-                    alt="main-pic"
-                    sizes="100vw"
-                    objectPosition="center 35%"
-                    className="shadow-md"
-                    
-                    
-                />
-            </div>
-            <div className="absolute inset-0 bg-black opacity-45"></div>
-            <div className="absolute top-1/2 right-0 transform translate-y-(-50%) text-right text-white right-[11%] px-4">
-                <h1 className="text-white text-4xl font-bold">Hello, my name is <span className="text-primary text-5xl ">Oscar Reyes</span></h1>
+
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white px-4">
+                
+                <h1 className="text-slate-400 text-5xl py-5 fade-up">
+                    Hello,
+                </h1>
+                <h2 className="text-slate-400 text-5xl fade-up-delay">
+                    my name is <span className="text-primary">Oscar Reyes</span>
+                </h2>
             </div>
         </section>
-
-       
-        
-
-        
-       
-    )
-
+    );
 }
