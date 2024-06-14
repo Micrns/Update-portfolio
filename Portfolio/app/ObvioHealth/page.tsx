b742c8a4703b1619492a4d946e9f1f429407def3
Oscar <ovreyes@uci.edu>
2024-06-13 23:49:41 -0700

    created new pages for work experience

---
+++ Portfolio/app/ObvioHealth/page.tsx
@@ -0,0 +1,11 @@
+import NavBar from "@/components/layout/Navigation";
+
+
+export default function ObvioHealthPage(){
+    return(
+        <section>
+            <NavBar />
+            <h1>ObvioHealth</h1>
+        </section>
+    )
+}
