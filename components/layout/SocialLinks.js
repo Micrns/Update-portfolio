f544edac5242f5d487dd0eb64572642c1d359a01
Oscar <ovreyes@uci.edu>
2024-06-27 23:42:12 -0700

    adjust size for about me

--- components/layout/SocialLinks.js
+++ components/layout/SocialLinks.js
@@ -20,7 +20,7 @@ export default function SocialLink() {
         </FadeInSection>
         <FadeInSection>
           <div className="text-wrap p-3 w-full md:w-10/12">
-            <h1 className="text-4xl md:text-7xl text-primary">About me</h1>
+            <h1 className="text-4xl md:text-3xl text-primary">About me</h1>
             <p className="w-full md:w-8/12 text-slate-400 text-left mt-2 md:mt-0">
               I am a recent graduate of the University of California, Irvine with a Bachelors of Science in Informatics, specializing in Human-Computer Interaction. My interest in programming and software development stems through the use of social media platforms by understanding how much of a benefit technology has in connecting people together. I want to build and create platforms that allow people to interact and network themselves with other people to provide an accessible environment through the use of software. This thought process motivated me to build personal projects to learn new languages and technologies and my short-term software engineering experience at ObvioHealth aided my basic understanding of software development. Besides programming, I am also interested in working out, fashion, and traveling.
             </p>
