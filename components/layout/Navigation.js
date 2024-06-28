239f313dc0fe6194962567e9c060bf090dcac98e
Oscar <ovreyes@uci.edu>
2024-06-27 23:23:43 -0700

    adjust icons to only imported ones to switch from react icons

--- components/layout/Navigation.js
+++ components/layout/Navigation.js
@@ -3,6 +3,8 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import Image from "next/image";
+// import { HiOutlineMenu, HiOutlineX } from 'react-icons/hi';
+
 export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -32,7 +34,20 @@ export default function NavBar() {
 
         <div className="md:hidden">
           <button onClick={toggleMenu} aria-label="Toggle Menu">
-            {isOpen ? <HiOutlineX size={24} /> : <HiOutlineMenu size={24} />}
+            {isOpen ? <Image
+              src="icons/ph_x.svg"
+              width={20}
+              height={20}
+              alt="Home Icon"
+              
+            /> : <Image
+            src="icons/mingcute_menu-fill.svg"
+            width={20}
+            height={20}
+            alt="Home Icon"
+            
+          />}
+          {/* {isOpen ? <HiOutlineX size={24} /> : <HiOutlineMenu size={24} />} */}
           </button>
         </div>
 
@@ -43,7 +58,7 @@ export default function NavBar() {
               src="icons/mdi_linkedin.svg"
               width={20}
               height={20}
-              alt="github Icon"
+              alt="LinkedIn Icon"
               
             />
           </Link>
@@ -69,7 +84,7 @@ export default function NavBar() {
               src="icons/ic_outline-email.svg"
               width={20}
               height={20}
-              alt="Home Icon"
+              alt="email icon"
               
             />
           </Link>
@@ -87,12 +102,26 @@ export default function NavBar() {
       </div>
 
       {isOpen && (
-        <nav className="md:hidden flex flex-col gap-4 mt-4">
+        <nav className="md:hidden flex flex-col items-center gap-4 mt-4">
           <Link href="https://www.linkedin.com/in/oscar-reyes1/">
-            <FaLinkedin className="text-color-linkedin hover:text-black" size={24} aria-label="LinkedIn" />
+            {/* <FaLinkedin className="text-color-linkedin hover:text-black" size={24} aria-label="LinkedIn" /> */}
+            <Image
+              src="icons/mdi_linkedin.svg"
+              width={20}
+              height={20}
+              alt="LinkedIn Icon"
+              
+            />
           </Link>
           <Link href="https://github.com/micrns">
-            <FaGithub className="text-color-github hover:text-black" size={24} aria-label="GitHub" />
+            {/* <FaGithub className="text-color-github hover:text-black" size={24} aria-label="GitHub" /> */}
+            <Image
+              src="icons/mdi_github.svg"
+              width={20}
+              height={20}
+              alt="github Icon"
+              
+            />
           </Link>
           <Link href="/Experience">
             <p className="text-primary hover:text-black cursor-pointer">Experience</p>
@@ -101,10 +130,24 @@ export default function NavBar() {
             <p className="text-primary hover:text-black cursor-pointer">About</p>
           </Link>
           <Link href="mailto:oscarvreyess@gmail.com">
-            <MdOutlineEmail className="text-primary hover:text-black" size={24} aria-label="Email" />
+            {/* <MdOutlineEmail className="text-primary hover:text-black" size={24} aria-label="Email" /> */}
+            <Image
+              src="icons/ic_outline-email.svg"
+              width={20}
+              height={20}
+              alt="email icon"
+              
+            />
           </Link>
           <Link href="/Resume/Oscar V Reyes's Resume.pdf">
-            <IoIosPaper className="text-primary hover:text-black" size={24} aria-label="Resume" />
+            {/* <IoIosPaper className="text-primary hover:text-black" size={24} aria-label="Resume" /> */}
+            <Image
+              src="icons/quill_paper.svg"
+              width={20}
+              height={20}
+              alt="Home Icon"
+              
+            />
           </Link>
         </nav>
       )}
--- components/layout/Skills.js
+++ components/layout/Skills.js
@@ -1,51 +1,38 @@
-import { SiTypescript } from "react-icons/si";
-import { FaReact } from "react-icons/fa";
-import { FaPython } from "react-icons/fa";
-import { FaNode } from "react-icons/fa";
-import { BsFiletypeSql } from "react-icons/bs";
-import { TiHtml5 } from "react-icons/ti";
-import { FaCss3Alt } from "react-icons/fa";
-import { IoLogoVue } from "react-icons/io5";
-import { FaDocker } from "react-icons/fa";
-import { SiNextdotjs } from "react-icons/si";
-import { SiTailwindcss } from "react-icons/si";
-import { FiFigma } from "react-icons/fi";
-import { IoLogoJavascript } from "react-icons/io5";
-import { SiCaddy } from "react-icons/si";
+"use client";
+import React from 'react';
 
 export default function SkillsList() {
     const skills = [
-        { component: SiTypescript, className: "text-blue-700", label: "TypeScript" },
-        { component: FaReact, className: "text-blue-400", label: "React" },
-        { component: FaPython, className: "text-slate-400", label: "Python" },
-        { component: FaNode, className: "text-green-600", label: "Node.js" },
-        { component: BsFiletypeSql, className: "text-blue-500", label: "SQL" },
-        { component: TiHtml5, className: "text-red-600", label: "HTML5" },
-        { component: FaCss3Alt, className: "text-sky-500", label: "CSS3" },
-        { component: IoLogoVue, className: "text-green-600", label: "Vue.js" },
-        { component: FaDocker, className: "text-blue-600", label: "Docker" },
-        { component: SiNextdotjs, className: "text-slate-500", label: "Next.js" },
-        { component: SiTailwindcss, className: "text-sky-400", label: "Tailwind CSS" },
-        { component: FiFigma, className: "text-red-700", label: "Figma" },
-        { component: IoLogoJavascript, className: "text-yellow-400", label: "JavaScript" },
-        { component: SiCaddy, className: "text-blue-400", label: "Caddy" },
+        { src: "icons/devicon_typescript.svg", alt: "TypeScript", label: "TypeScript" },
+        { src: "icons/logos_react.svg", alt: "React", label: "React" },
+        { src: "icons/logos_python.svg", alt: "Python", label: "Python" },
+        { src: "icons/fa6-brands_node.svg", alt: "Node.js", label: "Node.js" },
+        { src: "icons/logos_mysql.svg", alt: "SQL", label: "SQL" },
+        { src: "icons/logos_html-5.svg", alt: "HTML5", label: "HTML5" },
+        { src: "icons/logos_css-3.svg", alt: "CSS3", label: "CSS3" },
+        { src: "icons/logos_vue.svg", alt: "Vue.js", label: "Vue.js" },
+        { src: "icons/skill-icons_docker.svg", alt: "Docker", label: "Docker" },
+        { src: "icons/devicon_nextjs.svg", alt: "Next.js", label: "Next.js" },
+        { src: "icons/devicon_tailwindcss.svg", alt: "Tailwind CSS", label: "Tailwind CSS" },
+        { src: "icons/logos_figma.svg", alt: "Figma", label: "Figma" },
+        { src: "icons/logos_javascript.svg", alt: "JavaScript", label: "JavaScript" },
+        { src: "icons/simple-icons_caddy.svg", alt: "Caddy", label: "Caddy" },
     ];
 
     return (
-        <div className="text-3xl flex flex-col items-center gap-2">
-            {skills.map((skill, index) => {
-                const IconComponent = skill.component;
-                return (
-                    <div key={index} className="relative flex items-center group">
-                        <IconComponent
-                            className={`${skill.className} mb-2 transition-transform transform hover:scale-110`}
-                        />
-                        <span className="absolute left-full ml-2 bg-gray-800 text-white text-sm rounded px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                            {skill.label}
-                        </span>
-                    </div>
-                );
-            })}
+        <div className="text-2xl flex flex-col items-center gap-2">
+            {skills.map((skill, index) => (
+                <div key={index} className="relative flex items-center group">
+                    <img
+                        src={skill.src}
+                        alt={skill.alt}
+                        className="w-15 md:w-24 lg:w-60 mb-2 transition-transform transform hover:scale-110"
+                    />
+                    <span className="absolute left-full ml-2 bg-gray-800 text-white text-sm rounded px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                        {skill.label}
+                    </span>
+                </div>
+            ))}
         </div>
     );
 }
