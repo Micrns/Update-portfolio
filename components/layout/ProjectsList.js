800cd832835ed11f4c58ace246d0bd7dc92b2123
Oscar <ovreyes@uci.edu>
2024-07-04 01:53:18 -0700

    update

--- components/layout/ProjectsList.js
+++ components/layout/ProjectsList.js
@@ -82,7 +82,7 @@ export default function ProjectLists() {
       <Link href={'https://github.com/Micrns/Update-portfolio'}>
         <div className="imageContainer">
           <Image 
-            src={"/Sorting-algorithm.png"}
+            src={"\updatedPortfolio.png"}
             width={600}
             height={500}
             alt="weather"
