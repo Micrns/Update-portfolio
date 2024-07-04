cea29ccbd9011fc5c2060a580ccd96a8ba68d443
Oscar <ovreyes@uci.edu>
2024-07-04 01:18:15 -0700

    updated projectlist

--- components/layout/ProjectsList.js
+++ components/layout/ProjectsList.js
@@ -82,7 +82,7 @@ export default function ProjectLists() {
       <Link href={'https://github.com/Micrns/Update-portfolio'}>
         <div className="imageContainer">
           <Image 
-            src={"./new-portfolio.png"}
+            src={"/new-portfolio.png"}
             width={600}
             height={500}
             alt="weather"
